fd3ba29795196f71a874dc43ac481f2ed958c77c
Vitaly Raevsky <vitaly.raevsky@gmail.com>
2021-10-09 20:37:52 +0300

    add item fragment with layout

--- app/src/main/java/ru/skillfactory/rentateam/presentation/item/ItemFragment.kt
+++ app/src/main/java/ru/skillfactory/rentateam/presentation/item/ItemFragment.kt
@@ -5,12 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.squareup.picasso.Picasso
 import ru.skillfactory.rentateam.R
 import ru.skillfactory.rentateam.databinding.FragmentItemBinding
 
 class ItemFragment : Fragment(R.layout.fragment_item) {
 
     private var binding: FragmentItemBinding? = null
+    private val args: ItemFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +22,16 @@ class ItemFragment : Fragment(R.layout.fragment_item) {
         super.onCreateView(inflater, container, savedInstanceState)
         binding = FragmentItemBinding.inflate(inflater, container, false)
         return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.userName?.text = args.user.full_name
+        binding?.userEmail?.text = args.user.email
+        Picasso.get()
+            .load(args.user.avatarUrl)
+            .fit()
+            .into(binding?.userIcon)
     }
 
     override fun onDestroyView() {
