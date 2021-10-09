bebac3f675406533dc288c22c60bab3d3be433ec
Vitaly Raevsky <vitaly.raevsky@gmail.com>
2021-10-09 21:00:23 +0300

    split graphs and add about fragment

---
+++ app/src/main/java/ru/skillfactory/rentateam/presentation/about/AboutFragment.kt
@@ -0,0 +1,15 @@
+package ru.skillfactory.rentateam.presentation.about
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.skillfactory.rentateam.R
+
+class AboutFragment : Fragment(R.layout.fragment_about) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
