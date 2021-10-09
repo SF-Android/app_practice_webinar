0c9cb106551de74f5e4e0ffab3893a6ccb5a248a
Vitaly Raevsky <vitaly.raevsky@gmail.com>
2021-10-09 21:10:49 +0300

    resolve nav component

--- app/src/main/java/ru/skillfactory/rentateam/presentation/BaseActivity.kt
+++ app/src/main/java/ru/skillfactory/rentateam/presentation/BaseActivity.kt
@@ -2,13 +2,51 @@ package ru.skillfactory.rentateam.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import ru.skillfactory.rentateam.R
+import ru.skillfactory.rentateam.databinding.ActivityBaseBinding
 
 @AndroidEntryPoint
 class BaseActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBaseBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfig: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_base)
+        binding = ActivityBaseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        if(savedInstanceState == null) {
+            setupBottomNaviationBar()
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setupBottomNaviationBar()
+    }
+
+    private fun setupBottomNaviationBar() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfig = AppBarConfiguration(
+            setOf(
+                R.id.listFragment,
+                R.id.aboutFragment
+            )
+        )
+        binding.bottomNavView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfig)
     }
 }
